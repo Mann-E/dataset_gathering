44db290bb48f5bbdd516bee7937d5dfd88209d53
Muhammadreza Haghiri <haghiri75@gmail.com>
2024-10-25 00:10:47 +0330

    testing the api

--- dataset_api/app/controllers/instructions_controller.rb
+++ dataset_api/app/controllers/instructions_controller.rb
@@ -1,6 +1,11 @@
 class InstructionsController < ApplicationController
 
     def new
+        @instruction = Instruction.new(user: params[:user], assistant: params[:assistant])
+
+        if @instruction.save
+            render json: {"user" => @instruction.user, "assistant" => @instrcution.assistant}
+        end
     end
 
     def update
