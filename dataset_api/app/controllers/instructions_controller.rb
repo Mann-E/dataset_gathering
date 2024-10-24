e9d1acc6d67ed44c24a73ccfce645e6aa6c4932d
Muhammadreza Haghiri <haghiri75@gmail.com>
2024-10-25 00:15:44 +0330

    correct

--- dataset_api/app/controllers/instructions_controller.rb
+++ dataset_api/app/controllers/instructions_controller.rb
@@ -4,7 +4,7 @@ class InstructionsController < ApplicationController
         @instruction = Instruction.new(user: params[:user], assistant: params[:assistant])
 
         if @instruction.save
-            render json: {"user" => @instruction.user, "assistant" => @instrcution.assistant}
+            render json: {"user" => @instruction.user, "assistant" => @instruction.assistant}
         else
             render json: {"error" => "Oh, there was a problem"}, status: 500
         end
